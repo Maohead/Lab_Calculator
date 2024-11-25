bb8ab77c49d7cb461b99b0a67e08114661de3722
Maohead <folzy312@gmail.com>
2024-11-25 15:46:29 +0900

    create_splash_screen

--- app/src/main/java/com/example/lab_calculator/Splash_screen.kt
+++ app/src/main/java/com/example/lab_calculator/Splash_screen.kt
@@ -1,13 +1,17 @@
 package com.example.lab_calculator
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Splash_screen : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash_screen)
@@ -16,5 +20,9 @@ class Splash_screen : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        android.os.Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }, 2000)
     }
 }
